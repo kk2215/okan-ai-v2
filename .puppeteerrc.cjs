8d65da0bdd3b655c85473196191fac4b3a1707ab
kk2215 <kaitokk2002@gmail.com>
2025-07-28 15:11:43 +0900

    か

--- .puppeteerrc.cjs
+++ .puppeteerrc.cjs
@@ -4,6 +4,7 @@ const { join } = require('path');
  * @type {import("puppeteer").Configuration}
  */
 module.exports = {
-  // Changes the cache location for Puppeteer.
+  // Puppeteerがブラウザをダウンロード・保存する場所を変更します。
+  // これにより、Renderのようなサーバー環境でもブラウザを見つけられるようになります。
   cacheDirectory: join(__dirname, '.cache', 'puppeteer'),
 };
